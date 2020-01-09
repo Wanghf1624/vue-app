3086a849f3f2c350bbdcc442feba35bcbd4ce705
Wanghf1624 <1345625421@qq.com>
2020-01-09 10:30:28 +0800

    app ok

---
+++ src/views/manager/address/List.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+      <briup-fulllayout title="常用地址">
+      <!-- {{addresses}} -->
+      <van-list>
+        <van-cell
+          v-for="item in addresses"
+          :key="item.id"
+          :title="item.province+''+item.city+''+item.area+''+item.address"/>
+      </van-list>
+      <van-button block type="default" @click="toAddressEditHandler">添加</van-button>
+      </briup-fulllayout>
+  </div>  
+</template>
+<script>
+import { get } from "../../../http/axios";
+import { mapState } from "vuex";
+export default {
+   data(){
+     return{
+       addresses:[]
+     }
+   },
+   computed:{ //计算属性
+     //将状态机中的user对象的info对象获取到
+     ...mapState("user",["info"])
+   },
+   created(){
+     //调用加载地址信息的方法
+     this.loadAddress();
+   },
+   methods:{
+     //加载当前顾客的地址信息
+     loadAddress(){
+       let id = this.info.id;
+       let url = "/address/findByCustomerId?id="+id;
+       get(url).then((response)=>{
+         this.addresses = response.data;
+       })
+
+     },
+     //跳转到地址编辑
+     toAddressEditHandler(){
+       //编程跳转
+       this.$router.push("/manager/address_edit");
+     }
+   }
+}
+</script>
